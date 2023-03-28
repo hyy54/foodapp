05774f1afc6f276cff285d91537976a70c8da07a
心靈老鴨湯 <s1110631016@gms.nutc.edu.tw>
2023-03-28 13:54:05 +0800

    Initial Commit

---
+++ foodapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  foodapp
+//
+//  Created by Huang Yu Yun on 2023/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ foodapp/foodappApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodappApp.swift
+//  foodapp
+//
+//  Created by Huang Yu Yun on 2023/3/28.
+//
+
+import SwiftUI
+
+@main
+struct foodappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
